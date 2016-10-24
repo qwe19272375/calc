c4ab8152318cb82492ce3136ac3ba2d1943452bb
Brian Hsieh <qwe19272375@gmail.com>
2016-10-24 17:03:49 +0800

    Add .gitingore and new buttons, beautify everything.

--- script.js
+++ script.js
@@ -1,57 +1,65 @@
-$(document).ready(function(){
-	var testNumLength = function(number) {
+$(document).ready(function() {
+    var testNumLength = function(number) {
         if (number.length > 9) {
-            totaldiv.text(number.substr(number.length-9,9));
+            totaldiv.text(number.substr(number.length - 9, 9));
             if (number.length > 15) {
                 number = "";
                 totaldiv.text("Err");
             }
-        } 
+        }
     };
-	var number = "";
+    var number = "";
     var newnumber = "";
     var operator = "";
-    var radix = "10";
-    var newradix = "";
     var totaldiv = $("#total");
+    var hexdiv = $("#HEX");
+    var decdiv = $("#DEC");
+    var octdiv = $("#OCT");
+    var bindiv = $("#BIN");
     totaldiv.text("0");
-    $("#numbers a").not("#clear,#clearall").click(function(){
-		number += $(this).text();
-		totaldiv.text(number);
-		testNumLength(number);
+    hexdiv.text("0");
+    decdiv.text("0");
+    octdiv.text("0");
+    bindiv.text("0");
+    $("#numbers a").not("#clear,#clearall").click(function() {
+        number += $(this).text();
+        totaldiv.text(number);
+        testNumLength(number);
     });
-    $("#operators a").not("#equals").click(function(){
-		operator = $(this).text();
-		newnumber = number;
-		number = "";
-		totaldiv.text("0");
+    $("#operators a").not("#equals").click(function() {
+        operator = $(this).text();
+        newnumber = number;
+        number = "";
+        totaldiv.text("0");
     });
-    $("#radix a").click(function(){
-        newradix = $(this).attr("id");
-        totaldiv.text(parseInt(totaldiv.text(), parseInt(radix, 10)).toString(newradix));
-        radix = newradix;
-        newradix = "";
+    $("#clear,#clearall").click(function() {
+        number = "";
+        totaldiv.text("0");
+        hexdiv.text("0");
+        decdiv.text("0");
+        octdiv.text("0");
+        bindiv.text("0");
+        if ($(this).attr("id") === "clearall") {
+            newnumber = "";
+        }
     });
-    $("#clear,#clearall").click(function(){
-		number = "";
-		totaldiv.text("0");
-		if ($(this).attr("id") === "clearall") {
-			newnumber = "";
-		}
-    });
-    $("#equals").click(function(){
-    	if (operator === "+"){
-    		number = (parseInt(number, 10) + parseInt(newnumber,10)).toString(parseInt(radix,10));
-    	} else if (operator === "-"){
-    		number = (parseInt(newnumber, 10) - parseInt(number,10)).toString(parseInt(radix,10));
-    	} else if (operator === "/"){
-    		number = (parseInt(newnumber, 10) / parseInt(number,10)).toString(parseInt(radix,10));
-    	} else if (operator === "*"){
-    		number = (parseInt(newnumber, 10) * parseInt(number,10)).toString(parseInt(radix,10));
-    	}
-    	totaldiv.text(number);
-    	testNumLength(number);
-    	number = "";
-    	newnumber = "";
+    $("#equals").click(function() {
+        if (operator === "+") {
+            number = (parseInt(number, 10) + parseInt(newnumber, 10)).toString(10);
+        } else if (operator === "-") {
+            number = (parseInt(newnumber, 10) - parseInt(number, 10)).toString(10);
+        } else if (operator === "/") {
+            number = (parseInt(newnumber, 10) / parseInt(number, 10)).toString(10);
+        } else if (operator === "*") {
+            number = (parseInt(newnumber, 10) * parseInt(number, 10)).toString(10);
+        }
+        totaldiv.text(number);
+        hexdiv.text(parseInt(number, 10).toString(16));
+        decdiv.text(number);
+        octdiv.text(parseInt(number, 10).toString(8));
+        bindiv.text(parseInt(number, 10).toString(2));
+        testNumLength(number);
+        number = "";
+        newnumber = "";
     });
 });
